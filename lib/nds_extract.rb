7da7f5ade41d97a3892d520fdb1aa9d9376f8a9c
Brandon Green <brandon@brandonleeproductions.com>
2020-01-27 14:07:13 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -39,6 +39,7 @@ end
 def total_gross(source)
   # Write this implementation
   #
+  
   # Should use methods:
   # 1. directors_totals: returns a Hash of { dir_name => gross }
   # 2. list_of_directors: names provides an Array of directors names (use
